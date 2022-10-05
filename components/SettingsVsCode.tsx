5f1b580a79c0cbfb692a508ff8a1204c77e061b3
cmakhmud <98968434+cmakhmud@users.noreply.github.com>
2022-10-06 03:22:40 +0400

    Update SettingsVsCode.tsx

--- components/SettingsVsCode.tsx
+++ components/SettingsVsCode.tsx
@@ -1,8 +1,8 @@
 import React , {useState , useContext}from 'react'
 import { DataContext } from '../poviders/dataProvider';
-import CopyToClipboard from "react-copy-to-clipboard";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+// import CopyToClipboard from "react-copy-to-clipboard";
+// import SyntaxHighlighter from "react-syntax-highlighter";
+// import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 export default function SettingsVsCode() {
 
     const {code} = useContext<any>(DataContext) 
@@ -48,7 +48,7 @@ export default function SettingsVsCode() {
             
         </div>
         <button onClick={()=>getCode().then(response=>{setCodes(response.code)})} className='bg-[#7D4AEA] w-[184px] h-[60px] text-[18px] text-[#FFFF] font-[400] rounded-[12px]'>Get your Code</button>
-        <div className='w-[378px] h-[260px] bg-[#333333] rounded-[8px] p-5'><span className='text-white'><SyntaxHighlighter>{codes}</SyntaxHighlighter></span></div>
+        <div className='w-[378px] h-[260px] bg-[#333333] rounded-[8px] p-5'><span className='text-white'>{codes}</span></div>
         <span>Copy and paste the embed code above just before the closing body tag of your website template file.</span>
     </div>
   )
